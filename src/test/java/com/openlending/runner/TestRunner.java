2330df96fcac793737c723f58f6d262e610db633
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-09-18 01:47:55 -0500

    commit

--- src/test/java/com/openlending/runner/TestRunner.java
+++ src/test/java/com/openlending/runner/TestRunner.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {"html:target/cucumber-reports.html",
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
